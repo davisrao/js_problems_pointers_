cc4a77ad192d41b2f72ee04992077f07f994443c
davisrao <davisrao1@gmail.com>
2021-08-27 16:17:01 -0700

    completed two array object function

--- two-array-object.js
+++ two-array-object.js
@@ -1,3 +1,28 @@
+"use strict";
 // add whatever parameters you deem necessary
-function twoArrayObject() {
+
+/**
+ * inputs: two arrays: first is keys and second is values
+ * outputs: object that is the keys and values
+ * constraints: if there are not enought vlaues, rest of keys will be null.
+ * if not enough keys, ignore rest of values.
+ * 
+ * create an empty object at beginning of function
+ * loop through the array of keys and match 1:1 with the array of values
+ * return the object
+ */
+
+
+function twoArrayObject(keys, values) {
+    let resultObj = {};
+
+    for (let i = 0; i < keys.length; i++) {
+        if (values[i] === undefined) {
+            resultObj[keys[i]] = null;
+        }
+        else {
+            resultObj[keys[i]] = values[i];
+        }
+    }
+    return resultObj;
 }
